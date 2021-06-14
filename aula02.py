4e22bd63b8294bff7cb993ef7d8a1400c1b380d9
Nycole <nycole.xavierr@gmail.com>
2021-06-14 10:48:05 -0300

    print

--- aula02.py
+++ aula02.py
@@ -8,13 +8,13 @@ from numpy import int64
 data = pd.read_csv('datasets/kc_house_data.csv')
 
 # função que converte de objet (string) -> date
-#data['date']= pd.to_datetime(data['date'])
+data['date']= pd.to_datetime(data['date'])
 
 #mostra as primeiras seis linhas
-#print(data.head())
+print(data.head())
 
 # mostrar na tela os tipos de variáveis em casa coluna
-#print(data.dtypes)
+print(data.dtypes)
 
 # =======================================
 # Como converter os tipos de variáveis
@@ -49,29 +49,29 @@ data = pd.read_csv('datasets/kc_house_data.csv')
 # Deletar variáveis
 # =======================================
 
-#print(data.columns)
+print(data.columns)
 #cols =['nome_do_meigarom', 'comunidade_ds','data_abertura_comunidade_ds']
 #data= data.drop(cols, axis=1)
-#print(data.columns)
+print(data.columns)
 
 # =======================================
 # Forma 01: Direto pelos nome das columas
 # =======================================
 data = pd.read_csv('datasets/kc_house_data.csv')
-#print(data[['price', 'id', 'date']])
+print(data[['price', 'id', 'date']])
 
 # =============================================
 # Forma 02: Pelos indices das linhas e columas
 # =============================================
 # DADOS(linhas, colunas)
 # 'iloc' localizar de acordo com o index
-#print(data.iloc[0:10, 0:3])
+print(data.iloc[0:10, 0:3])
 
 # =============================================
 # Forma 03: Pelos indices das linhas e columas
 # =============================================
 # 'loc' localizar linhas e colunas
-#print(data.loc[0:10, ['price', 'id', 'date']])
+print(data.loc[0:10, ['price', 'id', 'date']])
 
 # =============================================
 # Forma 04: Indices Booleanos
@@ -79,7 +79,7 @@ data = pd.read_csv('datasets/kc_house_data.csv')
 # 1, 0
 # True, False
 #cols = [True, False, True, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]
-#print(data.loc[0:10, cols])
+print(data.loc[0:10, cols])
 
 # =============================================
 # Respondendo as perguntas de negócios
@@ -89,14 +89,14 @@ data = pd.read_csv('datasets/kc_house_data.csv')
 # 1. Qual a data do imóvel mais antigo do portifólio?
 # Ordenar o conjunto de dados pela menor data
 #data['date'] = pd.to_datetime(data['date'])
-#print(data.sort_values('date', ascending=True))
+print(data.sort_values('date', ascending=True))
     # R: 02-05-2014
 
 # 2. Quantos imóveis possuem máximo de andares?
 # Encontrar os números de andares e determinar o maior
 # Contar quantos imóveis eu tenho por andar
-#print(data['floors'].unique())
-#print(data[data['floors'] == 3.5].shape)
+print(data['floors'].unique())
+print(data[data['floors'] == 3.5].shape)
     # R: 8
 
 # 3. Criar uma classificação para os imóveis, separando-os em baixo e alto padrão, de acordo com preço.
@@ -115,21 +115,21 @@ report.to_csv('datasets/report_aula02.csv', index=False)
 
 # Plotly - Bibliotexa que armazena uma função que desenha mapa
 # Scatter MapBox - função que desenha o mapa
-# import plotly.express as px
+import plotly.express as px
 
-#data_mapa = data[['id', 'lat', 'long', 'price']]
-#mapa = px.scatter_mapbox(data_mapa, lat='lat', lon='long',
- #              hover_name= 'id',
- #              hover_data=['price'],
- #              color_discrete_sequence=['fuchsia'],
- #              zoom=3,
- #              height=300)
+data_mapa = data[['id', 'lat', 'long', 'price']]
+mapa = px.scatter_mapbox(data_mapa, lat='lat', lon='long',
+               hover_name= 'id',
+               hover_data=['price'],
+               color_discrete_sequence=['fuchsia'],
+               zoom=3,
+               height=300)
 
-#mapa.update_layout(mapbox_style = 'open-street-map')
-#mapa.update_layout(height= 600, margin={'r':0, 't':0,'l':0,'b':0})
-#mapa.show()
+mapa.update_layout(mapbox_style = 'open-street-map')
+mapa.update_layout(height= 600, margin={'r':0, 't':0,'l':0,'b':0})
+mapa.show()
 
-#mapa.write_html('datasets/mapa_house.html')
+mapa.write_html('datasets/mapa_house.html')
 
 # Exercícios
 
@@ -174,16 +174,16 @@ data['condition_type'] = 'standard'
 data.loc[data['condition'] <= 2, 'condition_type'] = 'bad'
 data.loc[(data['condition'] == 3) | (data['condition'] == 4), 'condition_type'] = 'regular'
 data.loc[data['condition'] == 5, 'condition_type'] = 'good'
-#print(data[['condition','condition_type']].head(50))
+print(data[['condition','condition_type']].head(50))
 
 
 # 04. Modifique o TIPO da coluna 'condition' para STRING
 #ver qual é o tipo
-#print(data.dtypes)
+print(data.dtypes)
 
 # Int -> STRING
 data['condition'] = data['condition'].astype(str)
-#print(data.dtypes)
+print(data.dtypes)
 
 # 05. Delete as colunas: 'sqft_living15' e 'sqft_lot15'
 
@@ -194,18 +194,18 @@ data = data.drop(cols, axis=1)
 # 06. Modifique o TIPO da coluna 'yr_build' para DATE
 
 #ver o tipo de coluna
-#print(data.dtypes)
+print(data.dtypes)
 
 # converter int64 -> DATE
 data['yr_built'] = pd.to_datetime(data['date'])
 
 # 7. Modifique o TIPO da coluna 'yr_renovated' para DATE
 # ver o tipo de coluna
-#print(data.dtypes)
+print(data.dtypes)
 
 # converter int64 -> DATE
 data['yr_renovated'] = pd.to_datetime(data['date'])
-#print(data.dtypes)
+print(data.dtypes)
 
 # 8. Qual a data mais antiga de construção de um imóvel?
 data[['yr_built']].sort_values('yr_built', ascending=True)
@@ -225,7 +225,7 @@ data[data['condition_type'] == 'regular'].shape
 
 # 12. Quantos imóveis estão com a condição igual a 'bad' e possuem 'vista para água'?]
 # deu essa mensagem de erro: TypeError: unsupported operand type(s) for &: 'float' and 'bool'
-#print(data.dtypes)
+print(data.dtypes)
 # condition_type = str
 # waterfront = int64
 
@@ -240,7 +240,6 @@ data[(data['condition_type'] == 'good') & (data['house_age'] == 'new_house')].sh
 # R: 1701
 
 # 14. Qual o valor do imóvel mais caro do tipo 'studio'?
-
 data[data['dormitory_type'] == 'studio'].sort_values('price', ascending=False)[['price','dormitory_type']]
 # R: 1247000
 
@@ -266,24 +265,22 @@ data[data['house_age'] == 'new_house'].sort_values('yr_renovated', ascending=Tru
 #========================================================================
 
 # 18. Selecione as colunas: 'id', 'date', 'price', 'floors', 'zipcode' pelo método:
-
     # 18.1 Direto pelo nome das colunas.
-#print(data[['id', 'date', 'price', 'floors', 'zipcode']])
+print(data[['id', 'date', 'price', 'floors', 'zipcode']])
 
     # 18.2 Pelos Índices
-#print(data.iloc[0:, 0:5])
+print(data.iloc[0:, 0:5])
 
     # 18.3 Pelos Índices das linhas e o nome das colunas
-#print(data.loc[0:, ['id', 'date', 'price', 'floors', 'zipcode']])
+print(data.loc[0:, ['id', 'date', 'price', 'floors', 'zipcode']])
 
     # 18.4 Índices Booleanos
-#print(data.columns)
+print(data.columns)
 #cols = [True, True, True, False, False, False, False, True, False, False, False, False, False, False, False, False, True, False, False, False, False, True, False, False]
-#print(data.loc[0: , cols])
+print(data.loc[0: , cols])
 
 # 19. Salve um arquivo .csv com somente a coluna do item 10 ao 17
 report.to_csv('datasets/question_aula02.csv', index=False)
-#report.to_csv('datasets/report_aula02.csv', index=False)
 
 # 20. Modifique a cor do ponto no mapa de pink para 'verde-escuro'
 
